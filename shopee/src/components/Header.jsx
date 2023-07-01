187d6b038e862295ad1fc7c45591d0c88623fe99
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-07-01 18:07:54 +0700

    shopee:fix header

--- shopee/src/components/Header.jsx
+++ shopee/src/components/Header.jsx
@@ -1,13 +1,37 @@
 import { SunIcon } from "@chakra-ui/icons";
 import {
+  Avatar,
   Box,
   Button,
   Divider,
   Flex,
   HStack,
+  IconButton,
+  Image,
+  Input,
+  Link,
+  List,
   Select,
   Text,
 } from "@chakra-ui/react";
+import data from "../data";
+
+function SearchBar(props) {
+  return (
+    <Flex maxW="60%" grow={1} direction="column">
+      <Flex>
+        <Input placeholder="dang ky va nhap vouch" />
+        <IconButton icon={<SunIcon />} />
+      </Flex>
+      <Flex justify="space-between">
+        <Link>Home</Link>
+        <Link>Home</Link>
+        <Link>Home</Link>
+        <Link>Home</Link>
+      </Flex>
+    </Flex>
+  );
+}
 
 function NavBar(props) {
   return (
@@ -43,6 +67,11 @@ export function Header(props) {
   return (
     <Box>
       <NavBar />
+      <Flex justify="space-around" align="center">
+        <Image src={data.shopeeLogo} />
+        <SearchBar />
+        <IconButton icon={<SunIcon />} />
+      </Flex>
     </Box>
   );
 }
---
+++ shopee/src/data.js
@@ -0,0 +1,3 @@
+const shopeeLogo =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe_O4LNcq2JdK54EaswEF__p8773qZx3wDXWfKSmEH_Q&s";
+export default { shopeeLogo };
